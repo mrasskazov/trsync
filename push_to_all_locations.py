0be1c533c9db3f2345c5b1d6c2393fc9f3595686
Max Rasskazov <mrasskazov@mirantis.com>
2015-06-25 19:35:34 +0300

    Implemented creating of addition symlinks during push_to_all_locations
    Change-Id: Ib207497fb69bc180dbac99761a055ac1a82a8f8c

--- push_to_all_locations.py
+++ push_to_all_locations.py
@@ -9,11 +9,13 @@ from trsync import TRsync
 servers = [_.strip() for _ in os.environ.get('LOCATIONS').split()]
 
 if len(sys.argv) < 3:
-    print 'Using: {} <source_dir>/ <target_mirror_name>'
+    print 'Using: {} <source_dir>/ <target_mirror_name> '\
+          '[<symlink_name>]..[<symlink_name>]'
     sys.exit(1)
 
 source_dir = sys.argv[1]
 mirror_name = sys.argv[2]
+symlinks = sys.argv[3:]
 
 failed = list()
 for server in servers:
@@ -22,7 +24,7 @@ for server in servers:
                     save_latest_days=365 * 2,
                     init_directory_structure=False)
     try:
-        remote.push(source_dir, mirror_name)
+        remote.push(source_dir, mirror_name, symlinks=symlinks)
     except Exception as e:
         print e.message
         failed.append(server)
