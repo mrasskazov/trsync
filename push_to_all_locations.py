46f5c64cad1c5274ae0109a1c016633c8017d2e5
Max Rasskazov <mrasskazov@mirantis.com>
2015-06-25 16:11:13 +0300

    Added push_to_all_locations.py
    Change-Id: I0a9306b6a49138d86d430ec6b4a970da6d560faa

---
+++ push_to_all_locations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+
+from trsync import TRsync
+
+servers = [_.strip() for _ in os.environ.get('LOCATIONS').split()]
+
+if len(sys.argv) < 3:
+    print 'Using: {} <source_dir>/ <target_mirror_name>'
+    sys.exit(1)
+
+source_dir = sys.argv[1]
+mirror_name = sys.argv[2]
+
+for server in servers:
+    source_dir += '/'
+    remote = TRsync(server,
+                    save_latest_days=365 * 2,
+                    init_directory_structure=False)
+    try:
+        remote.push(source_dir, mirror_name)
+    except Exception as e:
+        print e.message
