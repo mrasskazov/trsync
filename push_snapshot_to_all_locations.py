6ba2228baf083d0a4d271aa2d28f2e54178a43da
Max Rasskazov <mrasskazov@mirantis.com>
2015-07-02 21:43:47 +0300

    Implemented push_snapshot_to_all_locations.py utility that save timestamp from source snapshot
    Change-Id: I75e732ef18d857208fdf659cb31fa0f7f2a5a3d6

---
+++ push_snapshot_to_all_locations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+
+from trsync import TRsync
+
+servers = [_.strip() for _ in os.environ.get('LOCATIONS').split()]
+
+if len(sys.argv) < 3:
+    print 'Using: {} <source_dir> <target_mirror_name> '\
+          '[<symlink_name>]..[<symlink_name>]'
+    sys.exit(1)
+
+source_dir = sys.argv[1]
+mirror_name = sys.argv[2]
+symlinks = sys.argv[3:]
+
+failed = list()
+for server in servers:
+    source_dir = os.path.realpath(source_dir)
+    timestamp = source_dir[-17:]
+    if not source_dir.endswith('/'):
+        source_dir += '/'
+    remote = TRsync(server,
+                    timestamp=timestamp,
+                    save_latest_days=365 * 2,
+                    init_directory_structure=False)
+    try:
+        remote.push(source_dir, mirror_name, symlinks=symlinks)
+    except Exception as e:
+        print e.message
+        failed.append(server)
+
+if failed:
+    print "Failed to push to {}".format(str(failed))
+    sys.exit(1)
