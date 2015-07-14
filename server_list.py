11a1b553267c5ed6742405c339e1e2ea363bc328
Max Rasskazov <mrasskazov@mirantis.com>
2015-07-14 18:13:16 +0300

    Updated server_list to read list of servers from  env variable
    Change-Id: I64b5b6c9a86e449d2444997ed2b9410436e3374f

--- server_list.py
+++ server_list.py
@@ -1,8 +1,3 @@
-servers = [
-    'osci-mirror-msk.msk.mirantis.net::mirror-sync/mos',
-    'osci-mirror-kha.kha.mirantis.net::mirror-sync/mos',
-    'osci-mirror-srt.srt.mirantis.net::mirror-sync/mos',
-    'osci-mirror-poz.infra.mirantis.net::mirror-sync/mos',
-    'seed-cz1.fuel-infra.org::mirror-sync/mos',
-    'seed-us1.fuel-infra.org::mirror-sync/mos',
-]
+import os
+
+servers = [_.strip() for _ in os.environ.get('LOCATIONS').split()]
