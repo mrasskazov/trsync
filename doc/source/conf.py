cf8f6d849e76c008d6bf2114a7b2f9d43e7f9a8e
Max Rasskazov <mrasskazov@mirantis.com>
2016-06-09 00:10:07 +0300

    Fixed license header
    Change-Id: Ia33b2dcbb1d17f869e795dfb2c2bc3144969fed9

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2016, Mirantis, Inc.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -38,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'trsync'
-copyright = u'2013, OpenStack Foundation'
+copyright = u'2015-2016, Mirantis, Inc.'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
